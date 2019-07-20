c47af51fb724cd89c7bc212f3f70bfd248156464
Popoutsis <ajpopoutsis@gmail.com>
2019-07-19 23:35:57 -0500

    Update sensor with error if no valid authentication method exists

--- apps/check-config/checkconfig.py
+++ apps/check-config/checkconfig.py
@@ -10,10 +10,10 @@ class CheckConfig(hass.Hass):
 		# create a sensor to track check result
 		self.set_state("sensor.config_result", state="-", attributes = {"friendly_name": "Config Result", "detail": None})
 		
-		# get config data
+		# get HASS URL
 		self.apiurl = "{}/api/config/core/check_config".format(self.config["plugins"]["HASS"]["ha_url"])
 		
-		# token or key
+		# token or key to authenticate
 		if "token" in self.config["plugins"]["HASS"]:
 			self.auth = "token"
 			self.listen_state(self.check_config, "script.check_config")
@@ -22,14 +22,17 @@ class CheckConfig(hass.Hass):
 			self.listen_state(self.check_config, "script.check_config")
 		else:
 			self.log("AppDaemon config must use a token or key to authenticate with HASS")
+			self.set_state("sensor.config_result", state="ERROR", attributes = {"friendly_name": "Config Result", "detail": "AppDaemon config must use a token or key to authenticate with HASS"})
 		
 	def check_config(self, entity, attribute, old, new, kwargs):
+		# set headers for auth
 		if self.auth == "token":
 			self.headers = {'Authorization': "Bearer {}".format(self.config["plugins"]["HASS"]["token"])}
 		else: #key
 			self.headers = {'x-ha-access': self.config["plugins"]["HASS"]["ha_key"]}
+		# make the request
 		r = requests.post(self.apiurl, headers=self.headers)
-		self.log(r)
+		# evaluate result
 		if json.loads(r.text)['result'] == "valid":
 			self.set_state("sensor.config_result", state="valid", attributes = {"detail": None})
 			self.call_service("homeassistant/restart")
